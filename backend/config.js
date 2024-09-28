24d01fbc08000253e867abcb8c205d6b527db42a
rajarya199 <aryalrajan078@gmail.com>
2024-09-29 01:54:36 +0545

    mongoose db connect

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT=5555;
+ export const mongoURL='mongodb+srv://user1:admin123@bookstore-sample.myovb.mongodb.net/?retryWrites=true&w=majority&appName=BookStore-Sample'
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
 import express from 'express';
 import { PORT } from './config.js';
+import mongoose from "mongoose";
+import { mongoURL } from "./config.js";
+
 const app=express();
+
+ mongoose.connect(mongoURL)
+.then(()=>{
+    console.log('app is connected to db')
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+app.get('/',(request,response)=>{
+    console.log(request)
+    return response.status(234).send('welcome to mern stack project')
+})
 
 app.listen(PORT,()=>{
     console.log(`server is running at :${PORT}`)
