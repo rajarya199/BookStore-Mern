c000747512a25528ae47b46ba85867b990eaf38b
rajarya199 <aryalrajan078@gmail.com>
2024-09-30 23:07:51 +0545

    delete

--- backend/route/bookRoute.js
+++ backend/route/bookRoute.js
@@ -79,4 +79,20 @@ router.get('/detail/:id', async (request, response) => {
 
     }
   })
+  router.delete('/:id', async (request, response) => {
+    try {
+      const { id } = request.params;
+  
+      const result = await Book.findByIdAndDelete(id);
+  
+      if (!result) {
+        return response.status(404).json({ message: 'Book not found' });
+      }
+  
+      return response.status(200).send({ message: 'Book deleted successfully' });
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
 export default router
