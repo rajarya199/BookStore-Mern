8880253f145b88c6e3e45d7bc2d03873f1d7ec09
rajarya199 <aryalrajan078@gmail.com>
2024-10-01 22:58:05 +0545

    page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import ShowBook from './pages/ShowBook'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
 const App = () => {
   return (
-    <div className='bg-red-500 text-white'>
-      hello
-    </div>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/books/create' element={<CreateBooks/>}/>
+    <Route path='/books/delete/:id' element={<DeleteBook/>}/>
+    <Route path='/books/details/:id' element={<ShowBook/>}/>
+    <Route path='/books/edit/:id' element={<EditBook/>}/>
+   </Routes>
   )
 }
 
